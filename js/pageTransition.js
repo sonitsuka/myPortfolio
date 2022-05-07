a1ddb0d82dd0d414061b7b06344fe9cb4a2b48c4
sonitsuka <onitsukashiho@gmail.com>
2022-05-07 18:32:39 +0200

    ADD scrollTrigger & tailwindcss in Work

--- js/pageTransition.js
+++ js/pageTransition.js
@@ -12,3 +12,39 @@ pageTl.fromTo(".main-title", {opacity: 0}, {opacity:1, duration:1.2})
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
     speed:800
 });
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.defaults({
+    duration: 1.5,
+    opacity: 0,
+})
+
+let timeline01 = gsap.timeline({
+    ScrollTrigger: {
+        trigger: '#box1',
+        toggleAction:'restart pause none none',
+        start: 'top center',
+        markers:true
+    },
+})
+timeline01.from("#image01", {x: -150})
+timeline01.from("#text01", {y: 150}, "-=1")
+
+let timeline02 = gsap.timeline({
+    ScrollTrigger: {
+        trigger: "#box2",
+        start: 'top center'
+    },
+})
+timeline02.from('#image02', {x: 150})
+timeline02.from('#text02', {y: 150}, '-=1')
+
+let timeline03 = gsap.timeline({
+    ScrollTrigger: {
+        trigger: "#box3",
+        start: 'top center'
+    },
+})
+
+timeline03.from('#image03', {x: -150})
+timeline03.from('#text03', {y: 150}, '-=1')
