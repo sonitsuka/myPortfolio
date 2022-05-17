4122a563f40bdc42c875d95269bd31a5db5cb601
sonitsuka <onitsukashiho@gmail.com>
2022-05-17 12:35:25 +0200

    Fix sizing & corectlly display navbar in Safari

--- js/script.js
+++ js/script.js
@@ -63,24 +63,24 @@ let swiper = new Swiper (".work__container", {
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]');
+// const sections = document.querySelectorAll('section[id]');
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50;
+//         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=" + sectionId + "]').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=" + sectionId + "]').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=" + sectionId + "]').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=" + sectionId + "]').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
